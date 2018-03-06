07dc5f0729c4ef7c168d286ca03e6a82a69f54fa
yunren.lj <yunren.lj@alibaba-inc.com>
2018-03-06 11:36:38 +0800

    initialize project

--- lt-java/src/main/java/com/peterliu/lt/java/lock/MCSLock.java
+++ lt-java/src/main/java/com/peterliu/lt/java/lock/MCSLock.java
@@ -1,7 +1,92 @@
 package com.peterliu.lt.java.lock;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 /**
  * Created by liujun on 2018/3/6.
  */
-public class MCSLock {
+public class MCSLock implements Lock {
+
+    private AtomicReference<QNode> tail;
+    ThreadLocal<QNode> current;
+
+    public MCSLock(){
+        // 初始化为null
+        tail = new AtomicReference<QNode>(null);
+        current = new ThreadLocal<QNode>(){
+            @Override
+            protected QNode initialValue() {
+                return new QNode();
+            }
+        };
+    }
+
+    @Override
+    public void lock() {
+        // 获取当前节点
+        QNode lastOne = current.get();
+        QNode preOne = tail.getAndSet(lastOne);
+        if(preOne != null){
+            // 说明当前并不是最后一个节点, 需要自旋
+            lastOne.locked = true;
+            // 加入队列
+            preOne.next = lastOne;
+            // 自旋
+            while (lastOne.locked){
+                // 自旋转
+            }
+        }
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean tryLock() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void unlock() {
+        // 获取当前节点
+        QNode curr = current.get();
+        if(curr.next == null){
+            // 无后继节点
+            if(tail.compareAndSet(curr, null)){
+                // 清空tail
+                return;
+            }
+            // 设置失败，说明有新节点加入，等待新节点设置next
+            while (curr.next == null) {
+            }
+        }
+        // 唤醒后续节点
+        curr.next.locked = false;
+        curr.next = null;
+    }
+
+    @Override
+    public Condition newCondition() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * MCS队列节点
+     */
+    private static class QNode{
+        /*true表示该线程须要获取锁，且不释放锁；为false表示线程释放了锁*/
+        private volatile boolean locked = false;
+        private volatile QNode next;
+    }
 }
