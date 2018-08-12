06687d8d1e4a8c0051232263e458d4faa4e46841
’yunren.lj‘ <yunren.lj@alibaba-inc.com>
2018-08-12 22:55:46 +0800

    update

---
+++ lt-java/src/main/java/com/peterliu/lt/java/rx/FirstRxTest.java
@@ -0,0 +1,45 @@
+package com.peterliu.lt.java.rx;
+
+import com.peterliu.lt.common.AssistTools;
+import com.peterliu.lt.common.LocalLoggerFactory;
+import com.peterliu.lt.common.Logger;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+import java.io.IOException;
+
+/**
+ * Created by liujun on 12/08/2018.
+ */
+public class FirstRxTest {
+
+    private static Logger logger = LocalLoggerFactory.getLogger(FirstRxTest.class);
+
+    public static void main(String[] args) throws IOException {
+        Flowable.just("Hello World!").subscribe(System.out::println);
+        // no backpressure
+        Observable.create(emitter -> {
+            while (!emitter.isDisposed()) {
+                long time = System.currentTimeMillis();
+                emitter.onNext(time);
+                if (time % 2 != 0) {
+                    emitter.onError(new IllegalStateException("old time"));
+                    break;
+                }
+            }
+        })
+                .subscribe(System.out::println, logger::error);
+        // This allows you to defer the execution of the function you specify until a Subscriber subscribes to the Publisher.
+        // That is to say, it makes the function "lazy."
+        Flowable.fromCallable(() -> {
+            AssistTools.sleep(100);
+            return "done";
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.single())
+                .subscribe(System.out::println, logger::error);
+        System.in.read();
+    }
+}
